ea098ce6b9e7a15837161081d71bef22a86d02f5
438 <908504609@qq.com>
2018-02-07 23:55:48 +0800

    change flask to tornado

--- wzdt/webserver.py
+++ wzdt/webserver.py
@@ -6,7 +6,10 @@ import tornado.web
 import tornado.websocket
 import tornado.httpserver
 import tornado.ioloop
-import tornado-redis
+import tornadoredis
+
+# c = tornadoredis.Client(host='redis_server',password='kerwin')
+# c.connect()
 
 class IndexPageHandler(tornado.web.RequestHandler):
     def get(self):
@@ -19,16 +22,43 @@ class QuestionAnswerhandler(tornado.web.RequestHandler):
     def get(self):
         self.write('{"code": "SUCCESS", "data": {"answer": "C", "result": 0 }, "msg": "完成"}')
 
+class MessageHandler(tornado.websocket.WebSocketHandler):
+    def __init__(self, *args, **kwargs):
+        super(MessageHandler, self).__init__(*args, **kwargs)
+        self.listen()
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def __init__(self, *args, **kwargs):
+        super(WebSocketHandler, self).__init__(*args, **kwargs)
+        self.listen()
+
     def check_origin(self, origin):
         return True
 
-    def open(self):
-        pass
+    @tornado.gen.engine
+    def listen(self):
+        self.client = tornadoredis.Client(host='redis_server',password='kerwin')
+        self.client.connect()
+        yield tornado.gen.Task(self.client.subscribe, 'wzdt')
+        self.client.listen(self.on_message)
 
-    def on_message(self, message):
-        self.write_message(u"Your message was: "+message)
+    def open(self):
+        self.write_message(u"welcome wzdt answer system!" )
+
+    def on_message(self, msg):
+        #from web
+        if isinstance(msg, str):
+            self.write_message(msg)
+            return
+
+        #from reids
+        if (msg.kind == 'message'):
+            # self.send_data(str(msg.body))
+            self.write_message(str(msg.body))
+        elif ( msg.kind == 'unsubscribe'):
+            self.client.disconnect()
+            self.close()
+
 
     def on_close(self):
         pass
@@ -37,7 +67,7 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r'/', IndexPageHandler),
-            (r'/ws', WebSocketHandler),
+            (r'/wzdt', WebSocketHandler),
             (r'/question/answer', QuestionAnswerhandler),
             (r'/question/list', QuestionListhandler),
         ]
@@ -49,5 +79,5 @@ if __name__ == '__main__':
     ws_app = Application()
     server = tornado.httpserver.HTTPServer(ws_app)
     server.listen(5000)
-    print 'app is runing at 0.0.0.0:5000\nQuit the app with CONTROL-C'
+    print('app is runing at 0.0.0.0:5000\nQuit the app with CONTROL-C')
     tornado.ioloop.IOLoop.instance().start()
